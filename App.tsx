a2e807a9974b7fce9b2cb08348755cca4c2f25e9
Emersson Cardim Mota <63078277+CardimEmersson@users.noreply.github.com>
2020-08-07 21:42:53 -0300

    Primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import Landing from './src/pages/Landing';
+import { AppLoading } from 'expo';
+import { Archivo_400Regular, Archivo_700Bold, useFonts} from '@expo-google-fonts/archivo';
+import {Poppins_400Regular, Poppins_600SemiBold} from '@expo-google-fonts/poppins';
+import AppStack from './src/routes/AppStack';
 
+//instalar fonts do google
+//expo install expo-font @expo-google-fonts/archivo @expo-google-fonts/poppins
+
+//rotas
+//yarn add @react-navigation/native
+//expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+//navegação em stack/pilha
+//yarn add @react-navigation/stack
+//navagação em abas
+//yarn add @react-navigation/bottom-tabs 
+//yarn add axios
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  })
+  if(!fontsLoaded){
+    return <AppLoading/>
+  } else{
+    return (
+      //React fragment
+      <>   
+        <AppStack />  
+        <StatusBar style="light" />
+      </>
+     );
+  }
+ 
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
